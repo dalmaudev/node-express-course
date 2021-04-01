c6707b8fbfdd7a6f18c0ddbd360a8b471f765dc3
jdev <jdevunix@gmail.com>
2021-04-01 14:07:28 +0200

    add login POST route

--- server.js
+++ server.js
@@ -1,26 +1,58 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 const mockuserData = [{ name: "Mark" }, { name: "Jill" }];
 
-app.get("/users", (req, res) => {
-    res.json({
-      success: true,
-      message: "successfully got users, Nice!",
-      users: mockuserData,
-    });
-  });
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+// get all users
+app.get("/users", (req, res) => {
+  res.json({
+    success: true,
+    message: "successfully got users, Nice!",
+    users: mockuserData,
+  });
+});
+
+// get users by ID, with : we can use variables in params
 app.get("/users/:id", (req, res) => {
   console.log(req.params.id);
   res.json({
     success: true,
     message: "got one user",
-    user: req.params.id
-  })
-})
+    user: req.params.id,
+  });
+});
 
+// post login username and password
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
+});
 
 app.listen(8000, () => {
-    console.log("Server is running");
-  });
+  console.log("Server is running");
+});
